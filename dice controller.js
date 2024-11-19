1e246f25aca7f81f13c51e21667716129c5ec881
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2024-11-19 16:00:29 +0500

    now dice throwing works by an event listener

--- dice controller.js
+++ dice controller.js
@@ -1,3 +1,7 @@
+window.onload = function() {
+    document.getElementById("throw-d20-button").addEventListener("click", throwDice);
+}
+
 function throwDice(){
     let dice = new Dice(20);
     alert(dice.roll());
