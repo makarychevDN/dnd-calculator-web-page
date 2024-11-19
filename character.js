5d72f5e9bb822f8e2bf74a9ab293649f1e4a6c99
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2024-11-19 17:11:08 +0500

    a character class has been added

---
+++ character.js
@@ -0,0 +1,10 @@
+class Character{
+    constructor(strength, dexterity, constitution, intelligence, wisdom, charisma){
+        this._strength = strength;
+        this._dexterity = dexterity;
+        this._constitution = constitution;
+        this._intelligence = intelligence;
+        this._wisdom = wisdom;
+        this._charisma = charisma;
+    }
+}
