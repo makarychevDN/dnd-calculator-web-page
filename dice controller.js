4241cfcdefa237121b4e36aa7d41b93470882e66
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2024-11-20 16:03:02 +0500

    now it spawns dices buttons on throw

--- dice controller.js
+++ dice controller.js
@@ -1,12 +1,7 @@
-let d20dices;
-
 window.onload = function() {
     document.getElementById("throw-d20-button").addEventListener("click", throwD20);
     document.getElementById("throw-d20-with-advantage-button").addEventListener("click", throwD20WithAdvantage);
     document.getElementById("throw-d20-with-disadvantage-button").addEventListener("click", throwD20WithDisadvantage);
-    let dice1 =  new Dice(20);
-    let dice2 =  new Dice(20);
-    d20dices = [dice1, dice2];
 }
 
 function spawnDiceButton() {
@@ -17,37 +12,65 @@ function spawnDiceButton() {
     parent.appendChild(diceButton);
 }
 
+function spawnDiceButton(htmlParentId, text) {
+    let parent = document.getElementById(htmlParentId);
+    let diceButton = document.createElement("button");
+    parent.appendChild(diceButton);
+
+    diceButton.style = "my-button";
+    diceButton.textContent = text;
+}
+
 function displayResultOnLabel(value){
     document.getElementById("result-label").textContent = value;
 }
 
-function throwDices(dices){
+function rollDices(dices){
     dices.forEach(dice => {
         console.log(dice.roll());
+        spawnDiceButton("d20-dices-parent", dice.getCurrentValue());
     });
     
     return dices;
 }
 
 function throwD20(){
-    throwDices(d20dices);
-    let result = d20dices[0];
+    let dice =  new Dice(20);
+    let d20Dices = [dice];
+
+    d20Dices = rollDices(d20Dices);
+
+    //null sorting
+
+    let result = d20Dices[0];
     displayResultOnLabel(result.getCurrentValue());
     return result;
 }
 
 function throwD20WithAdvantage(){
-    throwDices(d20dices);
-    d20dices.sort((a, b) => b.getCurrentValue() - a.getCurrentValue());
-    let result = d20dices[0];
+    let dice1 =  new Dice(20);
+    let dice2 =  new Dice(20);
+    let d20Dices = [dice1, dice2];
+
+    d20Dices = rollDices(d20Dices);
+
+    d20Dices.sort((a, b) => b.getCurrentValue() - a.getCurrentValue());
+
+    let result = d20Dices[0];
     displayResultOnLabel(result.getCurrentValue());
     return result;
 }
 
 function throwD20WithDisadvantage(){
-    throwDices(d20dices);
-    d20dices.sort((a, b) => a.getCurrentValue() - b.getCurrentValue());
-    let result = d20dices[0];
+    let dice1 =  new Dice(20);
+    let dice2 =  new Dice(20);
+    let d20Dices = [dice1, dice2];
+
+    d20Dices = rollDices(d20Dices);
+
+    d20Dices.sort((a, b) => a.getCurrentValue() - b.getCurrentValue());
+
+    let result = d20Dices[0];
     displayResultOnLabel(result.getCurrentValue());
     return result;
 }
