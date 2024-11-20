62fc5ae6c2df0efa7e24008d33f5d00d5992b89d
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2024-11-20 19:16:58 +0500

    a dice roll events mechanism has been added

--- Dice.js
+++ Dice.js
@@ -1,16 +1,27 @@
 class Dice{
+    static dicesCount = 0;
+
     constructor(maxValue){
         this._maxValue = maxValue;
         this._currentValue = 0;
+
+        diceCount++;
+        this._rollEventName = `dice${diceCount}RollEvent`;
+        this._onRollEvenet = new CustomEvent(this._rollEventName, {detail: this._currentValue});
     }
 
     roll(){
         this._currentValue = getRandomInteger(1, this._maxValue);
+        dispatchEvent(this._onRollEvenet);
         return this._currentValue;
     }
 
     getCurrentValue(){
         return this._currentValue;
+    }
+
+    getUnicRollEventName(){
+        return this._rollEventName;
     }
 }
 
