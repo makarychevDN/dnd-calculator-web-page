a69a031db40a98ee965c4bee261052f7306c0256
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2024-11-20 19:44:49 +0500

    onLoad things have been moved to a new script

--- dice controller.js
+++ dice controller.js
@@ -1,9 +1,3 @@
-window.onload = function() {
-    document.getElementById("throw-d20-button").addEventListener("click", throwD20);
-    document.getElementById("throw-d20-with-advantage-button").addEventListener("click", throwD20WithAdvantage);
-    document.getElementById("throw-d20-with-disadvantage-button").addEventListener("click", throwD20WithDisadvantage);
-}
-
 function spawnDiceButton(htmlParentId, text) {
     let parent = document.getElementById(htmlParentId);
     let diceButton = document.createElement("button");
---
+++ onLoadConnecter.js
@@ -0,0 +1,5 @@
+window.onload = function() {
+    document.getElementById("throw-d20-button").addEventListener("click", throwD20);
+    document.getElementById("throw-d20-with-advantage-button").addEventListener("click", throwD20WithAdvantage);
+    document.getElementById("throw-d20-with-disadvantage-button").addEventListener("click", throwD20WithDisadvantage);
+}
