ae39a2ee6dd0c60d85ba20f3d531ec77f49d3d06
makarychevDN <39443709+makarychevDN@users.noreply.github.com>
2024-11-20 20:06:10 +0500

    now a dice controller has a commin API for each d20 throw

--- dice controller.js
+++ dice controller.js
@@ -11,6 +11,23 @@ function displayResultOnLabel(value){
     document.getElementById("result-label").textContent = value;
 }
 
+function throwD20(diceCount, sortingMode){
+    let d20Dices = [];
+    for(let i = 0; i < diceCount; i++){
+        d20Dices.push(new Dice(20));
+    }
+
+    d20Dices = rollDices(d20Dices);
+
+    if(sortingMode){
+        d20Dices.sort((a, b) => (a.getCurrentValue() - b.getCurrentValue()) * -sortingMode);
+    }
+
+    let result = d20Dices[0];
+    displayResultOnLabel(result.getCurrentValue());
+    return result;
+}
+
 function rollDices(dices){
     dices.forEach(dice => {
         console.log(dice.roll());
@@ -18,45 +35,4 @@ function rollDices(dices){
     });
     
     return dices;
-}
-
-function throwD20(){
-    let dice =  new Dice(20);
-    let d20Dices = [dice];
-
-    d20Dices = rollDices(d20Dices);
-
-    //null sorting
-
-    let result = d20Dices[0];
-    displayResultOnLabel(result.getCurrentValue());
-    return result;
-}
-
-function throwD20WithAdvantage(){
-    let dice1 =  new Dice(20);
-    let dice2 =  new Dice(20);
-    let d20Dices = [dice1, dice2];
-
-    d20Dices = rollDices(d20Dices);
-
-    d20Dices.sort((a, b) => b.getCurrentValue() - a.getCurrentValue());
-
-    let result = d20Dices[0];
-    displayResultOnLabel(result.getCurrentValue());
-    return result;
-}
-
-function throwD20WithDisadvantage(){
-    let dice1 =  new Dice(20);
-    let dice2 =  new Dice(20);
-    let d20Dices = [dice1, dice2];
-
-    d20Dices = rollDices(d20Dices);
-
-    d20Dices.sort((a, b) => a.getCurrentValue() - b.getCurrentValue());
-
-    let result = d20Dices[0];
-    displayResultOnLabel(result.getCurrentValue());
-    return result;
 }
--- onLoadConnecter.js
+++ onLoadConnecter.js
@@ -1,5 +1,5 @@
 window.onload = function() {
-    document.getElementById("throw-d20-button").addEventListener("click", throwD20);
-    document.getElementById("throw-d20-with-advantage-button").addEventListener("click", throwD20WithAdvantage);
-    document.getElementById("throw-d20-with-disadvantage-button").addEventListener("click", throwD20WithDisadvantage);
+    document.getElementById("throw-d20-button").addEventListener("click", function() { throwD20(1) });
+    document.getElementById("throw-d20-with-advantage-button").addEventListener("click", function() { throwD20(2, 1) });
+    document.getElementById("throw-d20-with-disadvantage-button").addEventListener("click", function() { throwD20(2, -1) });
 }
